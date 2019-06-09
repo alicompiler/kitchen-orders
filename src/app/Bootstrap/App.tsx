16cf3744d1d6a780d4973c8025ea60c89074a107
Ali <alihcompiler@gmail.com>
2019-06-09 15:27:59 +0300

    install core libs && build basic app structure

---
+++ src/app/Bootstrap/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+interface Props {
+}
+
+export default class App extends React.Component<Props> {
+
+    render() {
+        return (
+            <h1>
+                Kitchen-Orders System
+            </h1>
+        )
+    }
+
+}
