1240f3aefaab670ddb1843cc7e90bd0a9a7d3a2f
Ali <ali@Apples-MacBook-Pro.local>
2019-06-09 21:01:17 +0300

    design home page

--- src/app/SharedComponent/HomePage.tsx
+++ src/app/SharedComponent/HomePage.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import AppHeader from "./AppHeader";
+import {Link} from "react-router-dom";
 
 interface Props {
 
@@ -6,9 +8,23 @@ interface Props {
 
 export default class HomePage extends React.Component<Props> {
     render() {
+        const rand = Math.floor(Math.random() * 3) + 1;
+        const imagePath = `/images/home_bg_${rand}.svg`;
         return <div>
-            <button>Login As Admin</button>
-            <button>Login As User</button>
+            <AppHeader/>
+            <div style={{maxWidth: 1024, margin: 'auto'}}>
+                <img alt={'home background'} src={imagePath} className={'home_background'}/>
+
+
+                <br/><br/><br/><br/>
+
+                <div className={'login-buttons-container'}>
+                    <Link className={'admin-login-button'} to={'/login/admin'}>تسجيل الدخول كـ مدير</Link>
+                    <span style={{width: 24, height: 10}}/>
+                    <Link className={'user-login-button'} to={'/login/user'}>تسجيل الدخول كـ مستخدم</Link>
+                </div>
+
+            </div>
         </div>
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./app/Bootstrap/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
