6c4e252027cf49f8557af20218ad0b4d26120b8a
Ali <ali@Apples-MacBook-Pro.local>
2019-06-11 21:23:17 +0300

    improve orders list design && make fetch realtime

--- src/app/User/OrderList/OrderList.tsx
+++ src/app/User/OrderList/OrderList.tsx
@@ -6,6 +6,13 @@ import HorizontalLoader from "../../SharedComponent/HorizontalLoader/HorizontalL
 interface Props {
 }
 
+enum OrderStatus {
+    WAITING = 1,
+    ON_PROGRESS = 2,
+    DONE = 3,
+    REJECTED = 4,
+}
+
 export default class OrderList extends React.Component<Props, any> {
 
     constructor(props: Props) {
@@ -17,13 +24,13 @@ export default class OrderList extends React.Component<Props, any> {
         const db = firebase.firestore();
         const user = firebase.auth().currentUser as any;
 
-        db.collection("orders").where("userId", "==", user.uid).get().then((snapshot) => {
+        db.collection("orders").where("userId", "==", user.uid).onSnapshot((snapshot) => {
             const orders: any[] = [];
             snapshot.forEach((doc) => {
                 orders.push(doc.data());
             });
             this.setState({orders: orders, loading: false, error: false});
-        });
+        })
     }
 
 
@@ -35,18 +42,65 @@ export default class OrderList extends React.Component<Props, any> {
         }
 
         return <div>
-            <div className={'orders-list'}>
+            <div className={'orders-list'} style={{padding: 16}}>
                 {
-                    this.state.orders.map((order: any) => {
-                        return <div className={'order-list-item'}>
-                            <p>{order.user}</p>
-                            <p>{order.place}</p>
-                            <p>{order.status ? order.status : 'في الانتظار'}</p>
+                    this.state.orders.map(this.orderItem)
+                }
+            </div>
+        </div>
+    }
+
+    private orderItem = (order: any) => {
+        let bgStatus = '#EEE';
+        let colorStatus = '#454545';
+        let message = 'الطلب مرسل';
+        if (order.status === OrderStatus.ON_PROGRESS) {
+            bgStatus = '#F0F3BD';
+            colorStatus = '#222';
+            message = 'جاري العمل';
+        } else if (order.status === OrderStatus.DONE) {
+            bgStatus = '#02C39A';
+            colorStatus = '#FFF';
+            message = 'اكتمل'
+        } else if (order.status === OrderStatus.REJECTED) {
+            bgStatus = '#FF1654';
+            colorStatus = '#FFF';
+            message = 'مرفوض';
+        }
+
+        return <div className={'order-list-item'}>
+            <div className={'user-info'}>
+                <img alt={'avatar image'} src={'/images/user.png'}/>
+                <div className={'info'}>
+                    <p className={'name'}>{order.user}</p>
+                    <p className={'place'}>{order.place}</p>
+                    <span className={'status'} style={{
+                        background: bgStatus,
+                        color: colorStatus
+                    }}>{message}</span>
+                </div>
+            </div>
+            <div className={'items'}>
+                {
+                    order.items.map((item: any) => {
+                        return <div className={'item'}>
+                            <div>
+                                <span className={'count'}>
+                                    {item.count}
+                                </span>
+                            </div>
+                            <div className={'info'}>
+                                <div className={'name'}>{item.name}</div>
+                                {
+                                    item.note && <div className={'note'}>{item.note}</div>
+                                }
+                            </div>
                         </div>
                     })
                 }
             </div>
         </div>
     }
+
 
 }
