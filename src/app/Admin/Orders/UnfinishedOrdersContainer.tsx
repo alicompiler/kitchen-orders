38afa961fb93e04a60f0a168c6a75de3dbbcf20e
Ali <ali@Apples-MacBook-Pro.local>
2019-06-15 19:06:11 +0300

    fixing issues when fetch|update|delete item from query

--- src/app/Admin/Orders/UnfinishedOrdersContainer.tsx
+++ src/app/Admin/Orders/UnfinishedOrdersContainer.tsx
@@ -12,7 +12,6 @@ export default class UnfinishedOrdersContainer extends OrdersContainer {
             .where("status", "<", 100)
             .onSnapshot((snapshot) => {
                 snapshot.docChanges().forEach(change => {
-                    console.log(change);
                     if (change.type === "added") {
                         this.setState((state: any) => {
                             return {orders: [{...change.doc.data(), orderId: change.doc.id}].concat(state.orders)};
@@ -20,7 +19,15 @@ export default class UnfinishedOrdersContainer extends OrdersContainer {
                     } else if (change.type === "removed") {
                         this.setState((state: any) => {
                             const orders = [...state.orders];
-                            orders.splice(change.oldIndex, 1);
+                            const index = orders.length - 1 - change.oldIndex;
+                            orders.splice(index, 1);
+                            return {orders: [...orders]};
+                        });
+                    } else if (change.type === "modified") {
+                        this.setState((state: any) => {
+                            const orders = [...state.orders];
+                            const index = orders.length - 1 - change.oldIndex;
+                            orders[index] = {...change.doc.data(), orderId: change.doc.id};
                             return {orders: [...orders]};
                         });
                     }
--- src/app/User/OrderList/MyOrdersContainer.tsx
+++ src/app/User/OrderList/MyOrdersContainer.tsx
@@ -13,9 +13,25 @@ export default class MyOrdersContainer extends OrdersContainer {
             .where("userId", "==", user.uid)
             .onSnapshot((snapshot) => {
                 snapshot.docChanges().forEach(change => {
-                    this.setState((state: any) => {
-                        return {orders: [change.doc.data()].concat(state.orders)};
-                    });
+                    if (change.type === "added") {
+                        this.setState((state: any) => {
+                            return {orders: [{...change.doc.data(), orderId: change.doc.id}].concat(state.orders)};
+                        });
+                    } else if (change.type === "removed") {
+                        this.setState((state: any) => {
+                            const orders = [...state.orders];
+                            const index = orders.length - 1 - change.oldIndex;
+                            orders.splice(index, 1);
+                            return {orders: [...orders]};
+                        });
+                    } else if (change.type === "modified") {
+                        this.setState((state: any) => {
+                            const orders = [...state.orders];
+                            const index = orders.length - 1 - change.oldIndex;
+                            orders[index] = {...change.doc.data(), orderId: change.doc.id};
+                            return {orders: [...orders]};
+                        });
+                    }
                 });
                 this.setState({loading: false, error: false});
             })
