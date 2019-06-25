b7a99a204a273609dde1b357d58bdd81db7f7a5e
Ali <alihcompiler@gmail.com>
2019-06-25 10:40:29 +0300

    setup bgw firebase settings

--- src/app/Bootstrap/Firebase.ts
+++ src/app/Bootstrap/Firebase.ts
@@ -1,15 +1,15 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHRBQphohRg0i6y4UbjmNQEDlBmi4YzwQ",
-    authDomain: "kitchen-orders-78f0f.firebaseapp.com",
-    databaseURL: "https://kitchen-orders-78f0f.firebaseio.com",
-    projectId: "kitchen-orders-78f0f",
-    storageBucket: "kitchen-orders-78f0f.appspot.com",
-    messagingSenderId: "839850563122",
-    appId: "1:839850563122:web:c1adfb3429e79492"
+  apiKey: "AIzaSyD7ttOQvKVa_T51kBWaNDe21P-QC8Q0gqs",
+  authDomain: "kitchen-order-bgw.firebaseapp.com",
+  databaseURL: "https://kitchen-order-bgw.firebaseio.com",
+  projectId: "kitchen-order-bgw",
+  storageBucket: "kitchen-order-bgw.appspot.com",
+  messagingSenderId: "291598240312",
+  appId: "1:291598240312:web:ed4a36baea1b3c98"
 };
 
 firebase.initializeApp(firebaseConfig);
