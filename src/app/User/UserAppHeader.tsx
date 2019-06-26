4b7f9f292d6dd5c6edd7befba92a141d4259ecb2
= <=>
2019-06-26 06:38:01 +0300

    show logout only in admin page

--- src/app/User/UserAppHeader.tsx
+++ src/app/User/UserAppHeader.tsx
@@ -1,19 +1,26 @@
 import * as React from "react"
 import firebase from "./../Bootstrap/Firebase";
 
-export default class UserAppHeader extends React.Component {
+interface Props {
+    isAdmin?: boolean;
+}
+
+export default class UserAppHeader extends React.Component<Props> {
     render() {
         return <div className={'app-header'}>
             <div className={'container'}>
                 <div className={'app-header-content'}>
                     <div className={'logo-and-name'}>
-                        <img alt={'coffee cup'} src={'/images/logo.png'}/>
+                        <img alt={'coffee cup'} src={'/images/logo.png'} />
                         <p>ضيافة</p>
                     </div>
-                    <div className={'actions'} style={{cursor: 'pointer'}}>
-                        <span onClick={() => firebase.auth().signOut()}>
-                            <i className={'fas fa-sign-out-alt'} style={{fontSize: 24, color: '#FFF'}}/>
-                        </span>
+                    <div className={'actions'} style={{ cursor: 'pointer' }}>
+                        {
+                            this.props.isAdmin &&
+                            <span onClick={() => firebase.auth().signOut()}>
+                                <i className={'fas fa-sign-out-alt'} style={{ fontSize: 24, color: '#FFF' }} />
+                            </span>
+                        }
                     </div>
                 </div>
             </div>
